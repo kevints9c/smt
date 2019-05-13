36f857df4530ff85a826d346257583c2b36bf955
Administrator <Administrator@EDIANZU-R3G4PJR>
2019-05-13 22:06:55 +0800

    this is my smtTest commit 1

---
+++ smtTest/src/main/java/com/pingan/smt/smtTest/App.java
@@ -0,0 +1,13 @@
+package com.pingan.smt.smtTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
